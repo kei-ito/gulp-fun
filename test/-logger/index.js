a89370ef63bcb2de5af3b8887ef865b3fdd7bdec
Kei Ito <kei.itof@gmail.com>
2018-10-18 18:27:58 +0900

    upgeade @nlib/lint

--- test/-logger/index.js
+++ test/-logger/index.js
@@ -2,19 +2,19 @@ const {Writable} = require('stream');
 const WRITTEN = Symbol('written');
 module.exports = class Logger extends Writable {
 
-	constructor() {
-		const written = [];
-		super({
-			objectMode: true,
-			write(chunk, encoding, callback) {
-				written.push(chunk);
-				callback();
-			},
-		})[WRITTEN] = written;
-	}
+    constructor() {
+        const written = [];
+        super({
+            objectMode: true,
+            write(chunk, encoding, callback) {
+                written.push(chunk);
+                callback();
+            },
+        })[WRITTEN] = written;
+    }
 
-	get written() {
-		return this[WRITTEN].slice();
-	}
+    get written() {
+        return this[WRITTEN].slice();
+    }
 
 };
